cfebc092bf4d1d318befe49db41053c76ef08c10
hemraj4684 <h.solanki@wir-machen-druck.de>
2019-04-25 18:25:55 +0530

    Conflict resolved

--- app/Model/OptionData.php
+++ app/Model/OptionData.php
@@ -111,12 +111,9 @@ class OptionData extends Model
                 ->where('oce_id', $underlying->id)
                 ->where('date', $fetchdate)
                 ->first();
+
             //checks record already present or not if yes then return 0 otherwise return option_chain_expiry id
-<<<<<<< HEAD
             if (isset($underlyingData->id) && $underlyingData->id) {
-=======
-            if (isset($underlyingData->id)) {
->>>>>>> ecbf515f613649e287b1e645ff960f8362220a4e
                 return 0;
             } else {
                 return $underlying->id;

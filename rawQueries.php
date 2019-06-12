521da11c4e3a5667a70a651b4925e5eb121f829b
hemraj4684 <siya21211@gmail.com>
2019-06-12 08:10:25 +0530

    pcr query added

--- rawQueries.php
+++ rawQueries.php
@@ -8,3 +8,11 @@ SELECT avg(`open_interest`) FROM (SELECT * FROM oi_data WHERE symbol = 'infy' OR
 Q: to take option watchList
 SELECT date, oc.expirydate,strikeprice, callchnginoi,putchnginoi,calliv, putiv, ivratio, oc.symbol FROM `option_chain` join option_chain_expiry oc on oce_id = oc.id where watchlist = 1
 ORDER BY `date`  DESC
+
+Q:- to study option chain of particular stocks
+SELECT oc.date,oc.strikeprice,oc.callchnginoi,oc.putchnginoi,oc.calliv,oc.putiv,oc.ivratio,oc.callltp,oc.putltp FROM option_chain as oc join option_chain_expiry as oce on oc.oce_id = oce.id
+where oce.symbol = 'TCS' AND oce.expirydate = '2019-05-30' order by strikeprice,date
+
+Q:- CHECK particular script PCR
+SELECT oce.symbol, oce.expirydate, pcr.*  FROM `pcr` join option_chain_expiry oce on pcr.oce_id = oce.id  AND oce.symbol LIKE 'NIFTY'
+ORDER BY OCE.symbol , `pcr`.`id` DESC
